9b08e36917d937bdf6852897d685a9ab71489e0b
Alexandre <xandylambamg@hotmail.com>
2021-09-03 20:09:12 -0300

    adicionando div card

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -3,67 +3,76 @@
 @section('title', 'new_Lister')
 
 @section('content_header')
-<div class="conteudo container-fluid">
-    <h1 class="text-center">Bem vindo a sua lista de Compras</h1>
-@stop
+<div class="card">
+    <div class="conteudo container-fluid">
+        <h1 class="text-center">Bem vindo a sua lista de Compras</h1>
+    @stop
 
-@section('content')
+    @section('content')
 
-    <div class="text">
-        <form action="{{route('list.store')}}" method="post">
-            @csrf
+        <div class="text">
+            <form action="{{route('list.store')}}" method="post">
+                @csrf
 
-            <div class="input">
-                <input class="btn btn-dark"id="newtask" name="name" type="text" placeholder="-Produto"/>
-                <input class="btn btn-dark"name="quantidade" type="number" placeholder="-Quantidade"/>
-                <button class="btn btn-dark" onclick createList()>Cadastrar</button>
+                <div class="input">
+                    <input class="btn btn-dark"id="newtask" name="name" type="text" placeholder="-Produto"/>
+                    <input class="btn btn-dark"name="quantidade" type="number" placeholder="-Quantidade"/>
+                    <button class="btn btn-dark" onclick createList()>Cadastrar</button>
 
-            </div>
+                </div>
 
-        </form>
+            </form>
+        </div>
+    @if(!$data->isEmpty())
+        <div class="text-center col-md-12">
+            <table class="table table-sm col-md-12">
+                <div class="card-header">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>QUANTIDADE</th>
+                            <th>Usuario</th>
+                            <th>Ação</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $item)
+                                <tr>
+                                    <td> {{ $item->id }} </td>
+                                    <td> <input type="checkbox">{{ $item->name }} </td>
+                                    <td> {{ $item->quantidade }} </td>
+                                    <td> {{ $item->user->name }} </td>
+                                    <td>
+                                        <div class="button">
+                                            <form action="{{ route('list.destroy',$item->id)}}"  method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button id="excluir"class="btn btn-danger">Excluir</button>
+                                            </form>
+                                                <a href="{{ route('list.edit', $item->id) }}"><button id="update"class="btn btn-success">Edit</button></a>
+
+                </div>
+
+
+                                </div>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @else
+        <h1>Não Existe registro de lista</h1>
+    @endif
+    @stop
+
+
     </div>
-@if(!$data->isEmpty())
-    <div class="text-center col-md-12">
-        <table class="table table-sm col-md-12">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>QUANTIDADE</th>
-                <th>Usuario</th>
-                <th>Ação</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($data as $item)
-                    <tr>
-                        <td> {{ $item->id }} </td>
-                        <td> <input type="checkbox">{{ $item->name }} </td>
-                        <td> {{ $item->quantidade }} </td>
-                        <td> {{ $item->user->name }} </td>
-                        <td>
-                            <div class="button">
-                                <form action="{{ route('list.destroy',$item->id)}}"  method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button id="excluir"class="btn btn-danger">Excluir</button>
-                                </form>
-                                    <a href="{{ route('list.edit', $item->id) }}"><button id="update"class="btn btn-success">Edit</button></a>
-
-                            </div>
-
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-@else
-    <h1>Não Existe registro de lista</h1>
-@endif
-@stop
 
 </div>
+
 
 
 @section('css')
