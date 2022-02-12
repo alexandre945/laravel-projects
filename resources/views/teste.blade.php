224b13463f85666300bff7fc1172c1fdb3102886
Alexandre <Alexandresousaroberto@gmail.com>
2022-02-12 08:53:19 -0300

    corrigindo bug no modal

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'New_Lister')
 
 @section('content_header')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<div class="container-xl">
+<div class="container">
     @if ($errors->any())
     <div class="btn btn-warning">
         @foreach ($errors->all() as $error)
@@ -62,35 +64,62 @@
                                     <form action="{{ route('list.destroy',$item->id)}}" method="post">
                                         @method('DELETE')
                                         @csrf
-                                        <button id="excluir" class="btn btn-danger"
-                                            onclick="preventDefault()">Excluir</button>
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal{{$item->id}}">
+                                            Excluir
+                                        </button>
                                     </form>
                                     <a href="{{ route('list.edit', $item->id) }}"><button id="update"
                                             class="btn btn-success"> Edit</button></a>
 
-                                    <form action="{{ route('listtd.destroy',$item->id)}}" method="post">
+                                    <!-- <form action="{{ route('listtd.destroy',$item->id)}}" method="post">
                                         @method('DELETE')
                                         @csrf
-                                        <!-- <button id="excluir"class="btn btn-danger">Excl/Tud</button>   -->
-                                    </form>
+                                        <button id="excluir"class="btn btn-danger">Excl/Tud</button>  
+                                    </form> -->
                                 </div>
                             </td>
                         </tr>
+                        <form action="{{ route('list.destroy',$item->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <div class="modal fade" id="exampleModal{{$item->id}}" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header,btn btn-danger">
+                                            <h5 class="modal-title" id="exampleModalLabel">COMFIRMAÇÃO</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Tem certeza que deseja excluir este item?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-warning"
+                                                data-bs-dismiss="modal">Cancelar</button>
+                                            <button type="submit" class="btn btn-danger">Excluir</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                         @endforeach
                         @if($message = Session::get('success'))
                         <div class="alert alert-success alert-dismissible fade show" role="alert">
                             <strong>produto cadastrado com sucesso!</strong> {{ $message }}
-                            <!-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> -->
                         </div>
                         @endif
-
                     </tbody>
+
+
                 </table>
             </div>
 
         </div>
 
     </div>
+
     <div class="car-footer">
         <p>Desenvolvido por XandyDesenvolvimentoweb: (35)-998464219</p>
     </div>
@@ -101,6 +130,13 @@
         <li>AINDA NÃO EXISTE NEN UM CADASTADRO NA SUA LISTA</li>
     </div>
     @endif
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
     @stop
 </div>
 
